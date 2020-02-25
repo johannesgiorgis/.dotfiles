0f784687b14d2c88f3a2df5065f76e12a2971528
Johannes Giorgis <johannesgiorgis@users.noreply.github.com>
2020-02-25 09:35:15 -0800

    update install via brew script - new echo statements + install postgresql, cask install postman simplenote firefox

--- homebrew/install-via-brew.sh
+++ homebrew/install-via-brew.sh
@@ -13,15 +13,15 @@ print_stamp "$0 Started"
 brew update
 
 function brew_install {
-    print_stamp "Installing: ${@}..."
+    print_stamp "Installing via brew: '${@}'..."
     brew install "${@}"
-	print_stamp "Completed installing ${@}!"
+	print_stamp "Completed installing via brew: '${@}'!"
 }
 
 function brew_cask_install {
-    print_stamp "Installing: ${@}..."
+    print_stamp "Installing via brew cask: '${@}'..."
     brew cask install "${@}"
-	print_stamp "Completed installing ${@}!"
+	print_stamp "Completed installing via brew cask: '${@}'!"
 }
 
 # Basics
@@ -29,11 +29,14 @@ brew_install cask pipenv pyenv openssl \
     readline sqlite3 xz zlib tree entr \
     black flake8 htop wget watch \
     wifi-password tldr youtube-dl speedtest-cli \
-    jq
+    jq postgresql
 
 # Brew Cask
-brew_cask_install google-chrome iterm2 lepton sublime-text dropbox meld tunnelblick rectangle
+brew_cask_install google-chrome iterm2 lepton \
+    sublime-text dropbox meld tunnelblick rectangle \
+    postman simplenote firefox
 
+echo "Running 'brew upgrade' and 'brew cleanup'"
 brew upgrade
 brew cleanup
 
