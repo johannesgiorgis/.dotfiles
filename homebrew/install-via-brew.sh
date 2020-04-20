b89322df75f49033d9927f5a94d635cbb9739982
Johannes Giorgis <johannesgiorgis@users.noreply.github.com>
2020-04-20 11:41:56 -0700

    update install via brew script - add csvkit, remove xmlstarlet

--- homebrew/install-via-brew.sh
+++ homebrew/install-via-brew.sh
@@ -42,7 +42,7 @@ function brew_custom_install {
 
 # general tools
 brew_install cask htop jq speedtest-cli tldr tree watch wget wifi-password
-brew_install putty xmlstarlet
+brew_install putty csvkit
 
 # development
 brew_install black flake8 pipenv pyenv # python
