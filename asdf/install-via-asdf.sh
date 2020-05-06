a9b303530c1f5cc6c5b229649cb9959ce8232bb7
Johannes Giorgis <johannesgiorgis@users.noreply.github.com>
2020-05-05 17:51:31 -0700

    fix bug + clean up install via asdf script

--- asdf/install-via-asdf.sh
+++ asdf/install-via-asdf.sh
@@ -11,35 +11,35 @@
 export SUPPORT_DIR="${HOME}/.dotfiles/support"
 source "${SUPPORT_DIR}/common-utilities.sh"
 
-PLUGINS="python ruby nodejs golang"
-
 main() {
     start=$(date +%s)
-	echo "$LINE_BREAK"
-	start "$0 Started"
+    echo "$LINE_BREAK"
+    start "$0 Started"
 
     while read -r line
     do
         # Skip header file
         if [[ "${line}" == "#"* ]]
         then
-            # echo "Skipping header line..."
             continue
         fi
 
         plugin=$(echo "${line}" | cut -d' ' -f1)
         plugin_version=$(echo "${line}" | awk -F' ' '{print $2}')
 
-        print_info "PLUGIN:${plugin}|PLUGIN_VERSION:${plugin_version}"
-
-        # check if installed
-        if [[ $(asdf where "${plugin}" "${plugin_version}") != *"Version not installed"* ]]
-        then
-            warn "Plugin '${plugin} ${plugin_version}' already installed"
-            continue
-        fi
+        echo "-----------------------------------------------------------------"
+        print_info "› PLUGIN:${plugin}|PLUGIN_VERSION:${plugin_version}"
 
         asdf_add_plugin "${plugin}"
+
+        # check if installed
+        result=$(asdf where "${plugin}" "${plugin_version}" 2>&1)
+        print_info "› Result:^${result}^"
+        if [[ "${result}" != *"Version not installed"* ]]
+        then
+            warn "› Plugin '${plugin} ${plugin_version}' already installed"
+            continue
+        fi
 
         # TODO: Check against latest
         asdf_install_plugin "${plugin}" "${plugin_version}"
@@ -47,7 +47,7 @@ main() {
         asdf_set_global "${plugin}" "${plugin_version}"
     done < "${HOME}/.dotfiles/asdf/asdf-config"
 
-    print_info "Show 'asdf current' output:"
+    print_info "› Show 'asdf current' output:"
     asdf current
 
     end=$(date +%s)
@@ -60,18 +60,6 @@ main() {
 
 
 # ################### ASDF UTILITIES #################################
-
-function list_plugins() {
-    echo "${PLUGINS}" | tr ' ' '\n'
-}
-
-
-function add_plugins() {
-    asdf_add_plugin python
-    asdf_add_plugin ruby
-    asdf_add_plugin nodejs
-    asdf_add_plugin golang
-}
 
 
 function asdf_add_plugin() {
