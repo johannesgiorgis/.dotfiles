fe8429004545c934b35c6b30147270228768d3d6
Johannes Giorgis <johannesgiorgis@users.noreply.github.com>
2020-06-08 22:08:22 -0700

    add alias to charm for linux + add rust installs bin to PATH

---
+++ bin/andys-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+my_file="${HOME}/.config/term_counter"
+
+if [[ ! -e $my_file ]]; then
+	echo "0" > "$my_file"
+fi
+
+number_of_profiles=4
+term_counter=$(cat ${my_file})
+gnome-terminal --window-with-profile=RedPanda${term_counter}
+let term_counter="(term_counter + 1) % ${number_of_profiles}"
+echo "$term_counter" > "$my_file"
