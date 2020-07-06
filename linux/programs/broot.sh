60d4b8890c79c6c1e8034af184679b6b0ede43d1
Johannes Giorgis <johannesgiorgis@users.noreply.github.com>
2020-07-05 21:55:24 -0700

    add broot installation for linux + modify zsh to make br work

---
+++ linux/programs/broot.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Install broot
+# A better way to navigate directories
+# https://github.com/Canop/broot
+
+
+echo "Installing broot..."
+
+echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
+wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install broot --yes
+
+echo "Completed installing broot"
