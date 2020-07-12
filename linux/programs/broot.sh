b2a86dacbf4f89717597d164c1ded2d7dd68c70b
Johannes Giorgis <johannesgiorgis@users.noreply.github.com>
2020-07-12 15:11:00 -0700

    update linux programs installation: add brave-browser + update broot

--- linux/programs/broot.sh
+++ linux/programs/broot.sh
@@ -5,6 +5,12 @@
 # https://github.com/Canop/broot
 
 
+if dpkg -s broot &> /dev/null
+then
+    echo "INFO: broot is already installed"
+    exit 0
+fi
+
 echo "Installing broot..."
 
 echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
